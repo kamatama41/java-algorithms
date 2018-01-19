f512b446d7115478f90adf7916d768c55d699511
Shinichi Ishimura <shiketaudonko41@gmail.com>
2018-01-20 08:55:25 +0900

    Fix

--- src/main/java/com/kamatama41/algorithm/Exercise1_3.java
+++ src/main/java/com/kamatama41/algorithm/Exercise1_3.java
@@ -1,15 +1,14 @@
 package com.kamatama41.algorithm;
 
-import java.util.Scanner;
 
 public class Exercise1_3 {
 
     static int min4(int a, int b, int c, int d) {
-        int max = a;
-        if(b < max) max = b;
-        if(c < max) max = c;
-        if(d < max) max = d;
-        return max;
+        int min = a;
+        if(b < min) min = b;
+        if(c < min) min = c;
+        if(d < min) min = d;
+        return min;
     }
 
     public static void main(String[] args) {
