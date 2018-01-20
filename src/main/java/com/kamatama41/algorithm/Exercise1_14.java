ff852207645f3056250cb61065540541d1dfb0a5
Shinichi Ishimura <shiketaudonko41@gmail.com>
2018-01-20 13:14:05 +0900

    Fix

--- src/main/java/com/kamatama41/algorithm/Exercise1_14.java
+++ src/main/java/com/kamatama41/algorithm/Exercise1_14.java
@@ -7,7 +7,12 @@ public class Exercise1_14 {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
         System.out.println("正方形を表示します");
-        System.out.print("段数は: "); int n = stdIn.nextInt();
+
+        int n;
+        do {
+            System.out.print("段数は: "); n = stdIn.nextInt();
+        } while (n <= 0);
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print("*");
