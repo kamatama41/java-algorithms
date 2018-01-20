b9f6fb2b7bae83af447c0bda767e4a4c07867040
Shinichi Ishimura <shiketaudonko41@gmail.com>
2018-01-20 15:19:59 +0900

    Fix

--- src/main/java/com/kamatama41/algorithm/Exercise2_4.java
+++ src/main/java/com/kamatama41/algorithm/Exercise2_4.java
@@ -8,18 +8,18 @@ import static com.kamatama41.algorithm.Utils.randomIntArray;
 public class Exercise2_4 {
 
     static void copy(int[] a, int[] b) {
-        for (int i = 0; i < a.length; i++) {
-            b[i] = a[i];
+        for (int i = 0; i < b.length; i++) {
+            a[i] = b[i];
         }
     }
 
     public static void main(String[] args) {
         int n = 5;
         int[] a = randomIntArray(n);
-        int[] b = new int[n];
-        copy(a, b);
-
+        int[] b = randomIntArray(n);
         System.out.println(Arrays.toString(a));
         System.out.println(Arrays.toString(b));
+        copy(a, b);
+        System.out.println(Arrays.toString(a));
     }
 }
