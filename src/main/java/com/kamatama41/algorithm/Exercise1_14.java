c4487da768656576db82749ba7044ac586cc98d0
Shinichi Ishimura <shiketaudonko41@gmail.com>
2018-01-20 13:12:49 +0900

    Exercise 1-14

---
+++ src/main/java/com/kamatama41/algorithm/Exercise1_14.java
@@ -0,0 +1,18 @@
+package com.kamatama41.algorithm;
+
+import java.util.Scanner;
+
+public class Exercise1_14 {
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.println("正方形を表示します");
+        System.out.print("段数は: "); int n = stdIn.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
