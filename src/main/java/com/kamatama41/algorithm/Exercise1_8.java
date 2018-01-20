801f4191ba38b05abbff3cb35033974973b6dc55
Shinichi Ishimura <shiketaudonko41@gmail.com>
2018-01-20 11:05:08 +0900

    Exercise 1-8

---
+++ src/main/java/com/kamatama41/algorithm/Exercise1_8.java
@@ -0,0 +1,12 @@
+package com.kamatama41.algorithm;
+
+public class Exercise1_8 {
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        System.out.println("1から" + n + "までの和を求めます");
+        int sum = ((1 + n) * n) / 2;
+        System.out.println("1から" + n + "までの和は" + sum + "です。");
+    }
+}
