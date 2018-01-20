5f1211b834979cac5c57ced0894e32941038ccb5
Shinichi Ishimura <shiketaudonko41@gmail.com>
2018-01-20 13:25:59 +0900

    Exercise 1-15

---
+++ src/main/java/com/kamatama41/algorithm/Exercise1_15.java
@@ -0,0 +1,68 @@
+package com.kamatama41.algorithm;
+
+import java.util.Scanner;
+
+public class Exercise1_15 {
+
+    static void triangleLB(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void triangleLU(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void triangleRU(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j < i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void triangleRB(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j < n - i + 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.println("直角二等辺三角形を表示します");
+
+        int n;
+        do {
+            System.out.print("段数は: "); n = stdIn.nextInt();
+        } while (n <= 0);
+
+        triangleLB(n);
+        System.out.println();
+        triangleLU(n);
+        System.out.println();
+        triangleRU(n);
+        System.out.println();
+        triangleRB(n);
+    }
+}
