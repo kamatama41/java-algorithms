a35bd0f7e00c85d876544788e20076fa67570f51
Shinichi Ishimura <shiketaudonko41@gmail.com>
2018-01-20 13:28:10 +0900

    Fix

--- src/main/java/com/kamatama41/algorithm/Exercise1_15.java
+++ src/main/java/com/kamatama41/algorithm/Exercise1_15.java
@@ -24,12 +24,11 @@ public class Exercise1_15 {
 
     static void triangleRU(int n) {
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (j < i) {
-                    System.out.print(" ");
-                } else {
-                    System.out.print("*");
-                }
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= n; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
